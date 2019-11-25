148c1ba2181257e4d5b6987bd7237b187fbd29bf
TNDStudios <info@thenakeddeveloper.com>
2019-11-25 00:46:54 +0000

    Added Vue model and setup to allow for local model binding (MVVM)

---
+++ Receiver/wwwroot/js/telemetryModel.js
@@ -0,0 +1,40 @@
+﻿var tndStudios = tndStudios || {};
+tndStudios.models = tndStudios.models || {};
+tndStudios.models.telemetry =
+    {
+        // Telemetry Page Model
+        page: function () {
+
+            // The properties of the object            
+            this.applications = new tndStudios.models.telemetry.applications(null); // The applications object
+        },
+
+        // Applications Model
+        applications: function (data) {
+
+            // The list of applications
+            this.applicationArray = [];
+
+            // Copy the content of this list of applications from search json package
+            this.fromObject = function (fromObject) {
+
+                // Clear the object first (just in case)
+                this.clear();
+
+                // Start copying the data from the other object
+                this.applicationArray = fromObject;
+            }
+
+            // Clear this applications object (i.e. make it ready for use)
+            this.clear = function () {
+
+                // Clear the properties
+                this.applicationArray = [];
+            }
+
+            // Any data passed in?
+            if (data) {
+                this.fromObject(data); // Assign the data to this object
+            }
+        },
+    };
